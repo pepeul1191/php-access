60aa46b88d1c771f0e04fa05f7c9ae0f68bbd31e
Pepe Valdivia <jvaldivia@softweb.pe>
2019-04-13 18:42:48 -0500

    Correción de error template

--- src/configs/routes.php
+++ src/configs/routes.php
@@ -2,12 +2,9 @@
 
 use Slim\Http\Request;
 use Slim\Http\Response;
-use Controller\DepartamentoController;
-use Controller\DistritoController;
 use Controller\ErrorController;
 use Controller\HomeController;
 use Controller\LoginController;
-use Controller\ProvinciaController;
 
 // Routes
 $app->get('/demo/[{name}]', function (Request $request, Response $response, array $args) {
@@ -26,14 +23,6 @@ $app->get('/error/access/{numero}', ErrorController::class . ':access');
 //home
 $app->get('/', HomeController::class . ':view')->add($mw_session_true);
 //servicios REST
-$app->get('/departamento/listar', DepartamentoController::class . ':listar')->add($mw_ambiente_csrf);
-$app->post('/departamento/guardar', DepartamentoController::class . ':guardar')->add($mw_ambiente_csrf);
-$app->get('/provincia/listar/{departamento_id}', ProvinciaController::class . ':listar')->add($mw_ambiente_csrf);
-$app->post('/provincia/guardar', ProvinciaController::class . ':guardar')->add($mw_ambiente_csrf);
-$app->get('/distrito/listar/{provincia_id}', DistritoController::class . ':listar')->add($mw_ambiente_csrf);
-$app->post('/distrito/guardar', DistritoController::class . ':guardar')->add($mw_ambiente_csrf);
-$app->get('/distrito/buscar', DistritoController::class . ':buscar')->add($mw_ambiente_csrf);
-$app->get('/distrito/nombre/{distrito_id}', DistritoController::class . ':nombre')->add($mw_ambiente_csrf);
 // access
 $app->get('/access/', \Access\Controller\ViewController::class . ':index');
 // system
